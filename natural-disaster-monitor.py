d7a6e7f714002fb7c4b16b4e771d44588b51a93e
ShannonYuHsinLo <yuhsin.lo@sjsu.edu>
2026-05-03 19:11:43 -0700

    create: natural-disaster-monitor.py, requirements.txt, eonet_index.html
    create: file structure, templates/eonet_index.html

---
+++ natural-disaster-monitor.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+EONET_BASE = "https://eonet.gsfc.nasa.gov/api/v3"
+
+def fetch_events(days=30, category=None, status="open"):
+
+def fetch_categories():
+
+def analyze_events(events):
